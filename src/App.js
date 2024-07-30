9cabd621b2ead5811cfe8fe51cca481a190f9d29
vikashkt21 <vikashkatiyar966@gmail.com>
2024-07-30 20:29:44 +0530

    SignIn/SignUp Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-800">
-         netflix-gpt
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div >
+        <Body />
+      </div>
+    );
+  }
+  
+  export default App;
